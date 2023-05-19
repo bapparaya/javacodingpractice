acd756960afc51022a9e3fa973663acf8c42da90
bapparaya <boppamuthuraj78605@gmail.com>
2023-05-19 15:59:59 +0530

    reverse class change

--- src/javacodingpractice/EvennumberOddnumber.java
+++ src/javacodingpractice/EvennumberOddnumber.java
@@ -1,1 +1,17 @@
-//it is just a file need to check
+package javacodingpractice;
+
+public class EvennumberOddnumber {
+
+	public static void main(String[] args) {
+		
+		int number = 3;
+		
+		if(number%2==0) {
+			System.out.println("number is even number");
+		}
+		else {
+			System.out.println("number is odd number");
+		}
+	}
+	
+}
